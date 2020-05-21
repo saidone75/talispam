2488567048d5dddf7fbd235ac7d02faed6f71ec5
saidone <saidone@saidone.org>
2020-05-21 18:38:42 +0200

    new test

--- test/talispam/core_test.clj
+++ test/talispam/core_test.clj
@@ -1,3 +1,12 @@
 (ns talispam.core-test
   (:require [clojure.test :refer :all]
             [talispam.core :refer :all]))
+
+(let [config (talispam.config/load-config (talispam.utils/expand-home "~/.talispam/talispam.cfg.edn"))
+      dictionary (talispam.dictionary/load-dictionary!)
+      db (talispam.db/load-db)
+      message (rand-nth (talispam.corpus/ham))]
+  (deftest filter
+    (let [score (talispam.filter/score message)]
+      (is (<= 0 score))
+      (is (>= 1 score)))))
