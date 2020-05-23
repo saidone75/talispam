b16088863226510e16788f2adbdec749d06a67ce
saidone <saidone@saidone.org>
2020-05-23 10:26:34 +0200

    test to print stats from corpus

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [russellwhitaker/immuconf "0.3.0"]
                  [com.cognitect/transit-clj "1.0.324"]
                  [org.clojure/tools.cli "1.0.194"]
-                 [org.jsoup/jsoup "1.13.1"]]
+                 [org.jsoup/jsoup "1.13.1"]
+                 [com.stuartsierra/frequencies "0.1.0"]]
   :main ^:skip-aot talispam.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- test/talispam/core_test.clj
+++ test/talispam/core_test.clj
@@ -1,13 +1,20 @@
 (ns talispam.core-test
   (:require [clojure.test :refer :all]
-            [talispam.core :refer :all]))
+            [talispam.core :refer :all]
+            [com.stuartsierra.frequencies :as freq]))
 
 (let [config (talispam.config/load-config (talispam.utils/expand-home "~/.talispam/talispam.cfg.edn"))
       dictionary (talispam.dictionary/load-dictionary!)
       db (talispam.db/load-db)
       message (rand-nth (talispam.corpus/ham))]
+  ;; generic filter test
   (deftest filter-test
     (let [score (talispam.filter/score message)]
       (is (<= 0 score))
-      (is (>= 1 score)))))
-
+      (is (>= 1 score))))
+  ;; stats
+  (deftest stats
+    (def f-ham (freq/stats (frequencies (map talispam.filter/score (talispam.corpus/ham)))))
+    (def f-spam (freq/stats (frequencies (map talispam.filter/score (talispam.corpus/spam)))))
+    (println f-ham)
+    (println f-spam)))
